30631c4baa97f20ce1794b6decc850f8204f8235
isidioan <isidoros_ioannou@yahoo.com>
2019-02-13 00:13:31 +0200

    start moving to jpa

--- springbootjdbc/src/main/java/com/ioannou/springbootjdbc/entity/Person.java
+++ springbootjdbc/src/main/java/com/ioannou/springbootjdbc/entity/Person.java
@@ -1,13 +1,21 @@
 package com.ioannou.springbootjdbc.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.util.Date;
 
+@Entity
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class Person {
+    @Id
+    @GeneratedValue
     int id;
     String name;
     String location;
